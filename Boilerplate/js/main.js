65cf669af61e823f8e8157f9646c16cc3bafd20d
Hamza Qahoush <hamza.qah@gmail.com>
2022-03-25 18:43:10 +0300

    finsih models

--- Boilerplate/js/main.js
+++ Boilerplate/js/main.js
@@ -2,8 +2,74 @@
 // Later, you'll see how to organize your code into separate
 // files and modules.
 
-var Song = Backbone.Model.extend();
-var song = new Song({
-  title: "Aisha",
-  singer: "Shab Khaled",
+// var Song = Backbone.Model.extend();
+// var song = new Song({
+//   title: "Aisha",
+//   singer: "Shab Khaled",
+// });
+
+// validation part
+let Song = Backbone.Model.extend({
+  validate: function (attr) {
+    if (!attr.title) {
+      return "Title is required";
+    }
+  },
 });
+
+let song1 = new Song();
+
+// inhertance
+
+let Animal = Backbone.Model.extend({
+  walk: (o = () => console.log("animal is walking")),
+});
+
+let Dog = Animal.extend({
+  walk: function () {
+    Animal.prototype.walk.apply(this);
+    console.log("Dog is walking");
+  },
+});
+
+dog1 = new Dog();
+dog1.walk();
+
+let Vechicle = Backbone.Model.extend({
+  idAttribute: "registrationNumber",
+  validate: function (attrs) {
+    if (!attrs.registrationNumber) {
+      return "You cant create a vechicle without registrationNumber";
+    }
+  },
+
+  start: function () {
+    console.log("Vehicle started.");
+  },
+});
+
+let Car = Vechicle.extend({
+  start: function () {
+    console.log(
+      `Car with this registrationNumber ${this.get(
+        "registrationNumber"
+      )} started`
+    );
+  },
+});
+
+vechicle1 = new Vechicle();
+vechicle1.start();
+
+car1 = new Car();
+car1.set({ registrationNumber: "fdxr32r32", color: "blue" });
+car1.unset("registrationNumber");
+if (!car1.isValid()) {
+  console.log(car1.validationError);
+}
+car1.set({ registrationNumber: "dsfsdgfeg" });
+if (!car1.isValid()) {
+  console.log(car1.validationError);
+}
+
+car1.start();
--- README.md
+++ README.md
@@ -52,3 +52,87 @@ defaults:{
     genre : 'jazz'
 }
 ```
+
+### model Validation:
+
+- we can add validation to model by creating a methods e.g
+
+```
+let Song = Backbone.Model.extend({
+  validate: function(attr){
+    if (!attr.title) {
+      return "Title is required"
+    }
+  }
+})
+
+let song1 = new Song();
+
+```
+
+- we can check by `song1.isValid()`
+- to check the error and get it `song1.ValidationError`
+
+### Inheritance
+
+- we can extend our model by using extend Model.
+
+```
+let Animal = Backbone.Model.extend({
+  walk: (animal_ = () => console.log("animal is walking")),
+});
+
+let Dog = Animal.extend({
+walk: function () {
+    Animal.prototype.walk.apply(this);
+    console.log("Dog is walking");
+  },
+});
+
+dog1 = new Dog();
+dog1.walk();
+
+```
+
+- To access method of parnet Model we use :
+  `Animal.prototype.walk.apply(this)`
+
+### connecting to server:
+
+- Backbone model provide an abstraction of low level api communcation.
+- call fetch on your model , model take care of making AJax Call to get data.
+
+#### persistent operation :
+
+- fetch() - GET
+- save()- PUT/POST
+- destroy() - Delete
+
+- urlRoot :
+
+#### fetching Model
+
+```
+let Song=Backbone.Model.extend({
+urlRoot: '/api/songs'
+})
+var song=new Song({id : 1})
+song.fetch()
+
+will get :
+// GET/api/songs/1
+
+song.set('title' : 'newTitle')
+song.save()
+
+// PUT/api/songs/1
+
+//another syntax for save:
+
+song.save({},{
+
+  success: function(){}
+  error : function (){}
+})
+and pass the attr in the empty object
+```
